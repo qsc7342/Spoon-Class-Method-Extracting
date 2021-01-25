ba071cead39682ac5f1c356dc8c1c6e34f78b310
UmTaeho-Evaporation <nintendo7342@naver.com>
2021-01-26 03:34:20 +0900

    리팩토링

--- AST2CODE/src/main/java/SpoonTest.java
+++ AST2CODE/src/main/java/SpoonTest.java
@@ -23,15 +23,17 @@ import java.util.List;
 import java.util.*;
 
 class SpoonTest {
+    private static CtMethod method;
+
     public static Launcher getLaunch(String path) {
-        System.out.println("=================================");
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         System.out.println("Launching launcher...");
         Launcher launcher = new Launcher();
         CodeFactory codeFactory = new CodeFactory((launcher.createFactory()));
         launcher.addInputResource(path);
         launcher.buildModel();
         System.out.println("Done");
-        System.out.println("=================================");
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         return launcher;
     }
 
@@ -51,82 +53,50 @@ class SpoonTest {
         return model;
     }
 
+    public static Map<String, List<CtParameter> > getParams(Map<String, List<CtParameter> > par, CtMethod methods) {
+        String methodName = methods.getSimpleName();
+        if(par.containsKey(methodName)) {
+            par.get(methodName).addAll(new ArrayList<>(methods.getParameters()));
+        }
+        else {
+            par.put(methodName, new ArrayList<>(methods.getParameters()));
+        }
+        return par;
+    }
+
     public static Map<String, List<CtMethod> > getMethod(CtClass classes) {
         Map<String, List<CtMethod> > methods = new HashMap<>();
         String className = classes.getSimpleName();
-        methods.put(className, new ArrayList<>(classes.getMethods()));
+        if (methods.containsKey(className)) {
+            methods.get(className).addAll(new ArrayList<>(classes.getMethods()));
+        } else {
+            methods.put(className, new ArrayList<>(classes.getMethods()));
+        }
         return methods;
     }
 
-    public static Map<String, List<CtParameter> > getParams(CtMethod method) {
-        Map<String, List<CtParameter> > params = new HashMap<>();
-        params.put(method.getSimpleName(), new ArrayList<>(method.getParameters()));
-        return params;
-    }
+
     public static void main(String[] args) {
         Launcher launcher = getLaunch("C:/Users/ninte/Desktop/workspace");
-        Launcher launcher2 = getLaunch("C:/Users/ninte/Desktop/workspace2");
-//        try {
-//            Launcher launcher2 = getLaunch("C:/Users/ninte/Desktop/workspace2");
-//        }
-//        catch(SpoonException e) {
-//            /*
-//                Class 이름이 같을 경우 처리 필요
-//             */
-//            System.out.println("check");
-//            e.printStackTrace();
-//        }
-
-//        System.out.println("Build Model...");
         CtModel model = getModel(launcher);
-        CtModel model2 = getModel(launcher2);
-        System.out.println("Extract All Classes");
-        List<CtClass> classList = model.getElements(new TypeFilter<>(CtClass.class));
-        List<CtClass> classList2 = model2.getElements(new TypeFilter<>(CtClass.class));
-        System.out.print(classList.size());
-        System.out.println(" Classes in Project");
-        System.out.print(classList2.size());
-        System.out.println(" Classes in Project2");
-        for(CtClass ctclass: classList) {
-            String ref = ctclass.getReference().toString();
-            String[] path = ref.split("\\.");
-            System.out.println("Package : " + path[0] + "  Class : " + path[1]);
-        }
-
-        System.out.println("Extract All Methods...");
-        Map<String, List<CtMethod>> allMethods = new HashMap<>();
-        for (CtClass ctClass : classList) {
-            String cName = ctClass.getSimpleName();
-            System.out.println("CName : " + cName);
-            cName = cName.substring(0, cName.lastIndexOf("_") < 0 ? cName.length() : cName.lastIndexOf("_"));
-            if (allMethods.containsKey(cName)) {
-                allMethods.get(cName).addAll(new ArrayList<>(ctClass.getMethods()));
-            } else {
-                allMethods.put(cName, new ArrayList<>(ctClass.getMethods()));
-            }
-        }
-
-        /*
-            메소드 이름의 추출
-            mname에는 각 메소드의 바디까지 출력된다.
-            getSimpleName()을 이용해 메소드의 이름만을 추출 가능.
-         */
-        for(Map.Entry<String, List<CtMethod> > method: allMethods.entrySet()) {
-            System.out.println("============================================");
-            System.out.println("Class Name : " + method.getKey());
-            for(CtMethod mname: method.getValue()) {
-//                System.out.println("Reference : " + mname.getPath());
-                System.out.println("method : " + mname.getSimpleName());
-//                System.out.println("body : " + mname);
-                Map<String, List<CtParameter> > Params = new HashMap<>();
-                Params.put(mname.getSimpleName(), new ArrayList<>(mname.getParameters()));
-                for(Map.Entry<String, List<CtParameter> > param: Params.entrySet()) {
-                  System.out.println("Parameters : " + param.getValue());
+        List<CtClass> classList = getClass(model);
+        for(CtClass classes: classList) {
+            String[] ref = getRef(classes);
+            System.out.println("===========================================");
+            System.out.println("Package Name : " + ref[0]);
+            System.out.println("Class Name : " + ref[1]);
+            Map<String, List<CtMethod> > methods = getMethod(classes);
+            Map<String, List<CtParameter> > params = new HashMap<>();
+            for(Map.Entry<String, List<CtMethod> > method: methods.entrySet()) {
+                for(CtMethod current: method.getValue()) {
+                    params = getParams(params, current);
+                }
+                for(Map.Entry<String, List<CtParameter> > parameter: params.entrySet()) {
+                    System.out.println("Method Name : " + parameter.getKey());
+                    System.out.println("Parameter : " + parameter.getValue());
                 }
             }
-            System.out.println("============================================");
+            System.out.println("===========================================");
         }
-        System.out.println("Done");
-
     }
 }
